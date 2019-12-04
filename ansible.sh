bafe2c348b19f5074b8b880ec146263ad7e90043
mounikasamireddy <42339012+mounikasamireddy@users.noreply.github.com>
2019-12-04 11:53:49 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,4 +1,4 @@
-sudo apt update
-sudo apt install software-properties-common
+sudo apt-get update
+sudo apt-get install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install ansible
+sudo apt install ansible --yes
